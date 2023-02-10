0f96db9e41a0a49a03e5cc6de85b44acf6141d93
Kate L. Rosaldo <120040237+katerosaldo@users.noreply.github.com>
2023-02-10 14:21:59 +0800

    Add files via upload

---
+++ Exercise_5/Father.java
@@ -0,0 +1,8 @@
+public class Father {
+    protected String surname = "Rosaldo";
+
+    protected void showSurname(){
+        System.out.println("My family name is " + surname);
+        
+    }
+}
---
+++ Exercise_5/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private String name = "Keyt";
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+
+}
+}
